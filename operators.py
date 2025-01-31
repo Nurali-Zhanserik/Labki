20ee078cdb33dc7219c34d7044fbdfba09c4773b
Nurali Zhanserik <n_zhanserikuly@kbtu.kz>
2025-01-31 23:24:27 +0500

    inserting lab2

---
+++ operators.py
@@ -0,0 +1,29 @@
+# ARITHMETIC OPERATORS
+print(5+10) #this is simple addition operation in Python
+print(10-5) #this is simple subtraction in python
+#and so on
+print(10*5)
+print(10/5)
+print(10**2) # this is power
+print(10%3) # this takes the remainder
+print(10//3) # this takes the numbers that are not decimal
+
+print("------------------------------------")
+#ASSIGNMENT OPERATIONS
+x = 5
+x += 3
+print(x)
+#and so on
+
+print("----------------------------------------")
+#COMPARISON OPERATORS
+x = 5
+if x == 5: # this is comparison
+    print("x is actually equal to five")
+elif x != 5:
+    print("x is not equal to 5")  
+
+print("------------------")
+
+# paranthesis
+print((5+5)*3) #in this case paranthesis have precedence
